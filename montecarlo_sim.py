02d62646346db4ddc01c3b5b515fe454b00b8aa6
pedroarcoverde1 <p254719@dac.unicamp.br>
2025-08-17 22:53:37 -0300

    Montagem de uma simulação de Montecarlo(GBM) e verificações

---
+++ montecarlo_sim.py
@@ -0,0 +1,54 @@
+from simulation import simulate_gbm
+import matplotlib.pyplot as plt
+import numpy as np
+import yfinance as yf
+
+def main():
+    # fazer 10.000 simulações
+    num_simulations = 10000
+
+    # 1.Obter dados históricos
+    ticker = 'PETR4.SA'
+    data = yf.download(ticker, start='2020-01-01', end='2023-01-01')
+    log_returns = np.log(data['Close'] / data['Close'].shift(1))
+    log_returns.dropna(inplace=True)
+
+    # 2. Calcular o dift (mu) e a volatilidade (sigma)
+    mu = log_returns.mean()
+    mu = mu.item()
+    sigma = log_returns.std()
+    sigma = sigma.item()
+    print(f"Drift anual: {mu:.4f}")
+    print(f"Volatilidade anual: {sigma:.4f}")
+
+    # 3. Definir os parâmetros da simulação
+    S0 = data['Close'].iloc[-1] # Preço inicial é o ultimo preço conhecido
+    T = 1.0
+    dt = 1/252 # 1 dia de retorno
+    np.random.seed(42)
+
+    sim = simulate_gbm(S0, mu, sigma, T, dt, N_sim=num_simulations)
+    plt.figure(figsize=(10, 6))
+    plt.plot(sim[ : , : 100], linewidth=0.5, alpha=0.7)
+    plt.title(f'Simulação de Monte Carlo (GBM) para {ticker} (num_simulations={num_simulations})')
+    plt.xlabel('Dias de Negociação')
+    plt.ylabel('Preço')
+    plt.grid(True)
+
+    # calcular e plotar o preço final médio e intervalos de confiança
+    final_prices = sim[-1:]
+    mean_price = np.mean(final_prices)
+    ci_95_upper = np.percentile(final_prices, 97.5)
+    ci_95_lower = np.percentile(final_prices, 2.5)
+    print(f"Preço final médio: {mean_price:.2f}")
+    print(f"Intervalo de confiança 95%: ({mean_price - ci_95_lower:.2f}, {mean_price + ci_95_upper:.2f})")
+
+    # adicionar linhsa de estatísticas ao gráfico
+    plt.axhline(y = int(mean_price), color='red', linestyle='--', label='Preço final médio')
+    plt.axhline(y = int(ci_95_upper), color='green', linestyle=':', label='Intervalo de confiança 95%')
+    plt.axhline(y = int(ci_95_lower), color='green', linestyle=':', label='Intervalo de confiança 95%')
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
--- simulation.py
+++ simulation.py
@@ -18,33 +18,4 @@ def simulate_gbm(s0, mu, sigma, T, dt, N_sim = 1):
     for t in range(1, n_steps + 1):
         Z = np.random.standard_normal(size=N_sim)
         paths[t] = paths[t - 1] * np.exp((mu - 0.5 * sigma**2) * dt + sigma * np.sqrt(dt) * Z)
-        return paths
-
-
-# 1.Obter dados históricos
-ticker = 'PETR4.SA'
-data = yf.download(ticker, start='2020-01-01', end='2023-01-01')
-log_returns = np.log(data['Close'] / data['Close'].shift(1))
-log_returns.dropna(inplace=True)
-
-# 2. Calcular o dift (mu) e a volatilidade (sigma)
-mu = log_returns.mean()
-sigma = log_returns.std()
-print(f"Drift anual: {mu:.4f}")
-print(f"Volatilidade anual: {sigma:.4f}")
-
-# 3. Definir os parâmetros da simulação
-S0 = data['Close'][-1] # Preço inicial é o ultimo preço conhecido
-T = 1.0
-dt = 1/252 # 1 dia de retorno
-
-np.random.seed(42)
-sim = simulate_gbm(S0, mu, sigma, T, dt, N_sim=1)
-
-# 4. Plotar os resultados
-plt.figure(figsize=(10, 6))
-plt.plot(sim)
-plt.xlabel('Tempo')
-plt.ylabel('Preço')
-plt.title('Simulação de Preço do Ativo')
-plt.show()
+    return paths
